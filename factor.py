e1d2ac33c9b6afd4ae277edea057618e4983d52a
maidashabbir <151054210+maidashabbir@users.noreply.github.com>
2025-03-23 16:07:17 +0000

    Update factor.py

--- factor.py
+++ factor.py
@@ -130,14 +130,12 @@ def show_batch_comparison():
 
         # Radar chart
         radar_labels = df['Number'].astype(str).tolist()
-radar_values = df['Time'].tolist()
+        radar_values = df['Time'].tolist()
 
+        radar_labels += radar_labels[:1]
+        radar_values += radar_values[:1]
 
-radar_labels += radar_labels[:1]
-radar_values += radar_values[:1]
-
-angles = np.linspace(0, 2 * np.pi, len(radar_labels), endpoint=False).tolist()
-
+        angles = np.linspace(0, 2 * np.pi, len(radar_labels), endpoint=False).tolist()
 
         fig2, ax2 = plt.subplots(subplot_kw={'polar': True})
         ax2.plot(angles, radar_values, 'o-', linewidth=2)
